5466ea627835ccb08e7d3640874f0089beb784e1
varshaalex <varshamaria1501@gmail.com>
2025-01-14 20:27:15 -0500

    commit done

---
+++ string_example.js
@@ -0,0 +1,14 @@
+var s1 = new String('Hello World ')
+console.log(s1.toString())
+console.log(`Length: ${s1.length}`)
+console.log(`Upper: ${s1.toUpperCase()}`)
+console.log(`indexOf: ${s1.indexOf('W')}`)
+console.log(`Trim: ${s1.trim()}`)
+
+var s2 = "Seneca College"
+console.log(s2)
+console.log(`Length: ${s2.length}`)
+
+var s3 = String('Web Programming')
+console.log(s3)
+console.log(`Length: ${s3.length}`)
